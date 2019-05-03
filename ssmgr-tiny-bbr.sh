b27d3c6a5aaf00036a6d49832076896da3f47d46
atxiu <33857802+atxiu@users.noreply.github.com>
2019-05-04 03:29:29 +0800

    Update ssmgr-tiny-bbr.sh

--- ssmgr-tiny-bbr.sh
+++ ssmgr-tiny-bbr.sh
@@ -21,6 +21,7 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
 grub2-set-default 0
+grub2-mkconfig -o /boot/grub2/grub.cfg
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 #服务器优化
