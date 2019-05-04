8c1e59b57ee889c6c71ebf17eeda8c329f1aed22
atxiu <33857802+atxiu@users.noreply.github.com>
2019-05-04 17:48:40 +0800

    Update ssmgr-tiny-bbr.sh

--- ssmgr-tiny-bbr.sh
+++ ssmgr-tiny-bbr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 passwd=$(< /dev/urandom tr -dc 0-9-A-Z-a-z-|head -c "${1:-16}")
+#搬瓦工自家epel源删除了mbedtls、libsodium加密库
 sudo yum remove epel-release -y
 sudo yum install -y yum-fastestmirror yum-plugin-copr curl
 sudo yum copr enable librehat/shadowsocks -y
