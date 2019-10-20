1d055f7c5e013a4bbf305a762b555eda76c5e70e
atxiu <33857802+atxiu@users.noreply.github.com>
2019-10-21 07:15:39 +0800

    Update py-tiny.sh

--- py-tiny.sh
+++ py-tiny.sh
@@ -60,7 +60,7 @@ bbr()
     echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 }
-printf " install bbr yes or no"
+printf " install bbr"
 read plugin
 case $plugin in
     bbr)
