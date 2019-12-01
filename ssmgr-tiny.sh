62acceab008df500ba7fbaa326466767ec5df76f
atxiu <33857802+atxiu@users.noreply.github.com>
2019-12-01 11:42:23 +0800

    Update ssmgr-tiny.sh

--- ssmgr-tiny.sh
+++ ssmgr-tiny.sh
@@ -4,7 +4,7 @@ systemctl disable crond.service
 passwd=$(< /dev/urandom tr -dc 0-9-A-Z-a-z-|head -c "${1:-16}")
 #搬瓦工自家epel源删除了mbedtls、libsodium加密库
 sudo yum remove epel-release -y
-sudo yum install -y yum-fastestmirror yum-plugin-copr curl
+sudo yum install -y yum-fastestmirror yum-plugin-copr curl python-urllib3
 sudo yum copr enable librehat/shadowsocks -y
 sudo yum install -y epel-release
 sudo yum install shadowsocks-libev haveged git ntpdate -y
