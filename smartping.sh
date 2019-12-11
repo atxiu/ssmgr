75cb755937925b6e4653a1b2d0e39d8baa620acd
atxiu <33857802+atxiu@users.noreply.github.com>
2019-12-11 17:03:03 +0800

    Create smartping.sh

---
+++ smartping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir /root/smartping && cd /root/smartping
+wget https://github.com/smartping/smartping/releases/download/v0.8.0/smartping-v0.8.0.tar.gz
+tar zxvf smartping-v0.8.0.tar.gz
+./control start
+firewall-cmd --zone=public --add-port=8899/tcp --permanent
+firewall-cmd --reload
+chmod -R 755 /root/smartping/ 
+echo "/root/smartping/control start" >>/etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
