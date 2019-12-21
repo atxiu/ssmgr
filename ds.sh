aa6da3c0e207f9909d16e1dc049a18248994ee15
atxiu <33857802+atxiu@users.noreply.github.com>
2019-12-21 21:04:04 +0800

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -4,7 +4,7 @@ sport=$2
 method=$3
 passwd=$(< /dev/urandom tr -dc 0-9-A-Z-a-z-|head -c 16)
 yum install epel-release -y
-yum install docker docker-compose -y
+yum install docker docker-compose ntpdate -y
 systemctl start docker
 systemctl enable docker
 timedatectl set-timezone Asia/Shanghai
@@ -26,11 +26,12 @@ services:
   network_mode: host
   command: ssmgr -t s -s 127.0.0.1:${sport:-6601} -m 0.0.0.0:$[${sport:-6601}+1] -p $passwd -r libev:${method:-aes-128-gcm}
   volumes:
-      - ~/.ssmgr/${1:-ds}/s:/root/.ssmgr
+      - /root/.ssmgr/${1:-ds}/s:/root/.ssmgr
 EOF
 )>/root/.ssmgr/${folder:-ds}/docker-compose.yml
 /usr/bin/docker-compose -f /root/.ssmgr/${folder:-ds}/docker-compose.yml up -d
 chmod -Rf 755 /root/.ssmgr/
 echo "/usr/bin/docker-compose -f /root/.ssmgr/${folder:-ds}/docker-compose.yml up -d" >>/etc/rc.d/rc.local
+echo "/usr/sbin/ntpdate ntp1.aliyun.com" >>/etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 cat /root/.ssmgr/${folder:-ds}/docker-compose.yml
