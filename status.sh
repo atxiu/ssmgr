ed4cb9a0a40ac19448119bda5e43912bdf79e92f
atxiu <33857802+atxiu@users.noreply.github.com>
2020-01-19 01:25:43 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -3,6 +3,6 @@ lport=$1
 mkdir .status
 curl -o .status/client.py https://raw.githubusercontent.com/atxiu/ServerStatus/master/clients/client.py
 sed -i 's/status.botox.bz/status.ligu.tv/' /root/.status/client.py
-sed -i 's/s01/"${lport:-HK1}"/' /root/.status/client.py
+sed -i 's/s01/'${lport:-HK1}'/' /root/.status/client.py
 sed -i 's/35601/7891/' /root/.status/client.py
 sed -i 's/some-hard-to-guess-copy-paste-password/anjoy/' /root/.status/client.py
