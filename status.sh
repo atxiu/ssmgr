12cee4984c711f14044b5927800ed89935d623d4
atxiu <33857802+atxiu@users.noreply.github.com>
2020-01-19 01:30:36 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -6,3 +6,20 @@ sed -i 's/status.botox.bz/status.ligu.tv/' /root/.status/client.py
 sed -i 's/s01/'${lport:-HK1}'/' /root/.status/client.py
 sed -i 's/35601/7891/' /root/.status/client.py
 sed -i 's/some-hard-to-guess-copy-paste-password/anjoy/' /root/.status/client.py
+chmod 755 .status/client.py
+(
+cat <<EOF
+[Unit]
+Description=ServerStatus Client
+After=network.target
+
+[Service]
+IgnoreSIGPIPE=no
+ExecStart=/root/.status/client.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)>/lib/systemd/system/status.service
+systemctl restart status.service
+systemctl enable status.service
