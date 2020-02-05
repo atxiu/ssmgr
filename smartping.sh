1c9354033325d922f22bbe6d5d7ba0dbb14814e4
atxiu <33857802+atxiu@users.noreply.github.com>
2020-02-06 04:37:36 +0800

    Update smartping.sh

--- smartping.sh
+++ smartping.sh
@@ -4,6 +4,7 @@ mkdir /root/smartping
 cd /root/smartping
 wget https://github.com/smartping/smartping/releases/download/v0.8.0/smartping-v0.8.0.tar.gz
 tar zxvf smartping-v0.8.0.tar.gz -C /root/smartping
+cd /root/smartping
 ./control start
 firewall-cmd --zone=public --add-port=8899/tcp --permanent
 firewall-cmd --reload
