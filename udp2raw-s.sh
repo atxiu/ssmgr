44eb800c017cd23b99ded8993c99d23394741d74
atxiu <33857802+atxiu@users.noreply.github.com>
2020-03-18 21:12:47 +0800

    Create udp2raw-s.sh

---
+++ udp2raw-s.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+pwd=$(pwd)
+folder=$1
+lport=$2
+rport=$[rport-1]
+tun=$[rport-8800]
+mkdir $pwd/.udp2raw
+mkdir $pwd/.udp2raw/$folder
+(
+cat <<EOF >$pwd/.udp2raw/$folder/docker-compose.yml
+# docker-compose.yml
+version: '3'
+services:
+  tinyvpn:
+    image: atxiu/tinyfecvpn
+    restart: always
+    command: "-s \
+      -l 127.0.0.1:$rport \
+      --mode 0  \
+      --mtu 1200 \
+      -f 2:0 \
+      --timeout 0 \
+      --disable-obscure \
+      --disable-checksum \
+      --tun-dev tun1$tun \
+      --sub-net 10.11.$tun.0 \
+      --sock-buf 10240 \
+      --mssfix 0"
+    network_mode: "host"
+    cap_add:
+      - NET_ADMIN
+    devices:
+      - /dev/net/tun:/dev/net/tun
+  udp2raw-aes:
+    image: atxiu/udp2raw-aes
+    restart: always
+    command: "-s \
+      -k anjoy \
+      -l 0.0.0.0:$lport \
+      -r 127.0.0.1:$rport \
+      -a \
+      --raw-mode faketcp \
+      --cipher-mode aes128cfb \
+      --auth-mode hmac_sha1 \
+      --seq-mode 3 \
+      --sock-buf 10240 \
+      --force-sock-buf \
+      --keep-rule \
+      --fix-gro"
+    network_mode: "host"
+    cap_add:
+      - NET_ADMIN
+EOF
+)
