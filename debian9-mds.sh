ea84aaff36c880c152840e11008441952f6f1bd3
atxiu <33857802+atxiu@users.noreply.github.com>
2020-03-18 15:55:23 +0800

    Update debian9-mds.sh

--- debian9-mds.sh
+++ debian9-mds.sh
@@ -5,12 +5,12 @@ method=$3
 pwd=$(pwd)
 passwd=$(< /dev/urandom tr -dc 0-9-A-Z-a-z-|head -c 16)
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
