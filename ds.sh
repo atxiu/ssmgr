08aed3b89d039abf459c3e2ec47d8b9108889bca
atxiu <33857802+atxiu@users.noreply.github.com>
2020-06-05 04:47:48 +0800

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -12,11 +12,19 @@ cat <<EOF
 # docker-compose.yml
 version: '3'
 services:
- ${folder:-ds}:
-  image: gyteng/ssmgr-tiny
-  restart: always
-  network_mode: host
-  command: node /ssmgr/index.js -s 127.0.0.1:${sport:-6601} -m 0.0.0.0:$[${sport:-6601}+1] -p $passwd -r libev:${method:-aes-128-gcm} -d ./data.json
+  ${folder:-ds}:
+    image: gyteng/ssmgr-tiny
+    restart: always
+    network_mode: host
+    command: node /ssmgr/index.js -s 127.0.0.1:${sport:-6601} -m 0.0.0.0:$[${sport:-6601}+1] -p $passwd -r libev:${method:-aes-128-gcm} -d ./data.json
+    deploy:
+      resources:
+        limits:
+          cpus: '0.25'
+          memory: 200M
+        reservations:
+          cpus: '0.1'
+          memory: 50M
 EOF
 )>$pwd/.ssmgr/${folder:-ds}/docker-compose.yml
 /usr/local/bin/docker-compose -f $pwd/.ssmgr/${folder:-ds}/docker-compose.yml up -d
