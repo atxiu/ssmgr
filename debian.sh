e5015ab528d83b800cedf92d47452a58cb13c0d4
atxiu <33857802+atxiu@users.noreply.github.com>
2021-08-19 23:37:37 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 curl -sSL https://get.docker.com | sh
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-(
-cat <<EOF >/etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-exit 0
-EOF
-)
-chmod +x /etc/rc.local
-systemctl start rc-local.service
-systemctl enable rc-local.service
 (
 cat <<EOF >> /etc/sysctl.conf
 ####################################
