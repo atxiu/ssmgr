67f7f2c68f914556e43c140bf9ab573bc95f410f
atxiu <33857802+atxiu@users.noreply.github.com>
2021-12-10 18:13:21 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sSL https://get.docker.com | sh
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L https://get.daocloud.io/docker/compose/releases/download/v2.2.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 (
